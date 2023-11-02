aea62ef941ee48cfeec0d5c287bb9faa627e9160
Eprieto99 <149719728+Eprieto99@users.noreply.github.com>
2023-11-02 16:56:25 -0400

    Updated Sensors.ino void.loop

--- Sensors.ino
+++ Sensors.ino
@@ -25,6 +25,43 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+// put your main code here, to run repeatedly:
+ 
+  Serial.begin(9600);
+  long duration_front, distance_front, duration_left, distance_left, duration_right, distance_right;
+    
+// Calculating distance
+  
+  digitalWrite(trigger_front, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger_front, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger_front, LOW);
+  duration_front = pulseIn(echo_front, HIGH);
+  distance_front= duration_front*0.034/2;
+
+  digitalWrite(trigger_left, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger_left, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger_left, LOW);
+  duration_left = pulseIn(echo_left, HIGH);
+  distance_left= duration_left*0.034/2;
+
+  digitalWrite(trigger_right, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger_right, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger_right, LOW);
+  duration_right = pulseIn(echo_right, HIGH);
+  distance_right= duration_right*0.034/2;
+
+  Serial.print("front = ");
+  Serial.println(distance_front);
+  Serial.print("Left = ");
+  Serial.println(distance_left);
+  Serial.print("Right = ");
+  Serial.println(distance_right);  
+  delay(50);
 
 }
